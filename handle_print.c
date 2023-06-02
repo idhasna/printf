2370587032315a9acd60ffc261100bc31da16e42
houmaima <hourioumaima@gmail.com>
2023-06-01 20:10:35 -0700

    printf team project

--- handle_print.c
+++ handle_print.c
@@ -16,15 +16,15 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'i', print_int}, {'d', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexa},
-		{'X', print_Hexa}, {'p', print_pointer}, {'S', print_non_printable},
-		{'r', print_rev}, {'R', print_rot13}, {'\0', NULL}
+		{'c', write_char}, {'s', write_string}, {'%', write_percent},
+		{'i', write_num}, {'d', write_int}, {'b', write_binary},
+		{'u', write_unsigned}, {'o', write_octal}, {'x', write_hexa},
+		{'X', print_Hexa}, {'p', print_pointer}, {'S', write_non_printable},
+		{'r', write_rev}, {'R', write_rot13}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+			return (fmt_types[i].fun(list, buffer, flags, width, precision, size));
 
 	if (fmt_types[i].fmt == '\0')
 	{
